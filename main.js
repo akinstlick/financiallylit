21038d2eb44f45e0421f94701f7e3f78b09438f3
akinstlick <ajkinstlick@gmail.com>
2022-03-01 21:35:59 -0600

    update to sync with linux servers

--- main.js
+++
@@ -1,5 +0,0 @@
-//8 colors
-var colors = ["#FF355E","#FF6037","#FF9933","#FFFF66","#66FF66","#50BFE6","#EE34D2","#FF00CC"];
-var idx = Math.floor(Math.random()*8);
-var str = "background-color: " + colors[idx]
-document.getElementsByTagName('html')[0].setAttribute('style',str)
